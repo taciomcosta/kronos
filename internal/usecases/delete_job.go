93f1c0fbcb6952dde5894802d803a4d33829d57b
Tacio Costa <taciomcosta@gmail.com>
2021-02-16 15:16:07 -0300

    feat: fix delete job not unscheduling

--- internal/usecases/delete_job.go
+++ internal/usecases/delete_job.go
@@ -1,5 +1,9 @@
 package usecases
 
+import (
+	"github.com/taciomcosta/kronos/internal/entities"
+)
+
 // DeleteJobResponse represents response of delete job usecase
 type DeleteJobResponse struct {
 	Msg string `json:"msg"`
@@ -7,10 +11,22 @@ type DeleteJobResponse struct {
 
 // DeleteJob deletes a job given its name
 func DeleteJob(name string) (DeleteJobResponse, error) {
-	_, err := reader.FindOneJob(name)
+	job, err := reader.FindOneJob(name)
 	if err != nil {
 		return DeleteJobResponse{}, err
 	}
 	_ = writer.DeleteJob(name)
+	unscheduleJob(job)
 	return DeleteJobResponse{Msg: name + " deleted."}, nil
+}
+
+func unscheduleJob(job entities.Job) {
+	var index int
+	for i := range jobs {
+		if jobs[i].Name == job.Name {
+			index = i
+			break
+		}
+	}
+	jobs = append(jobs[:index], jobs[index+1:]...)
 }
