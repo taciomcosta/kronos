a32ea05bc9faf731477e50417e5fe14102fdb5a4
Tacio Costa <taciomcosta@gmail.com>
2021-02-16 14:42:46 -0300

    feat: add DELETE jobs handler

--- internal/interfaces/rest/jobs.go
+++ internal/interfaces/rest/jobs.go
@@ -4,22 +4,29 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/taciomcosta/kronos/internal/usecases"
+	uc "github.com/taciomcosta/kronos/internal/usecases"
 )
 
 // CreateJob handles jobs creation request.
 func CreateJob(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	var jobRequest usecases.CreateJobRequest
+	var jobRequest uc.CreateJobRequest
 	err := ReadJSON(r.Body, &jobRequest)
 	if err != nil {
-		respondJSONBadRequest(w, err)
+		respondError(w, err)
 	}
-	response, err := usecases.CreateJob(jobRequest)
+	response, err := uc.CreateJob(jobRequest)
 	respond(w, response, err)
 }
 
 // FindJobs handles finding all jobs request.
 func FindJobs(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	jobs := usecases.FindJobs()
+	jobs := uc.FindJobs()
 	respond(w, jobs, nil)
+}
+
+// DeleteJob handles deleting a job
+func DeleteJob(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	name := ps.ByName("name")
+	response, err := uc.DeleteJob(name)
+	respond(w, response, err)
 }
--- internal/interfaces/rest/rest.go
+++ internal/interfaces/rest/rest.go
@@ -2,9 +2,11 @@ package rest
 
 import "github.com/julienschmidt/httprouter"
 
+// NewRouter creates a new router with endpoints set
 func NewRouter() *httprouter.Router {
 	router := httprouter.New()
 	router.POST("/jobs", CreateJob)
 	router.GET("/jobs", FindJobs)
+	router.DELETE("/jobs/:name", DeleteJob)
 	return router
 }
