25547ee018a39e72853901cc8dbff06258460512
Tacio Costa <taciomcosta@gmail.com>
2021-02-20 12:22:39 -0300

    build: update install.bash

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 SOURCE_DAEMON=com.taciomcosta.kronos.plist
-TARGET_DAEMON=/Library/LaunchDaemons/com.taciomcosta.kronos.plist
+TARGET_DAEMON=/Library/LaunchAgents/com.taciomcosta.kronos.plist
 SOURCE_KRONOSD=../build/kronosd
 TARGET_KRONOSD=/usr/local/bin/kronosd
 SOURCE_KRONOSCLI=../build/kronos
 TARGET_KRONOSCLI=/usr/local/bin/kronos
 
-cp $SOURCE_DAEMON $TARGET_DAEMON
-cp $SOURCE_KRONOSD $TARGET_KRONOSD
-cp $SOURCE_KRONOSCLI $TARGET_KRONOSCLI
+sudo mkdir /etc/kronos
+sudo mkdir /var/lib/kronos
+
+sudo cp $SOURCE_DAEMON $TARGET_DAEMON
+sudo cp $SOURCE_KRONOSD $TARGET_KRONOSD
+sudo cp $SOURCE_KRONOSCLI $TARGET_KRONOSCLI
 
 launchctl load $TARGET_DAEMON
