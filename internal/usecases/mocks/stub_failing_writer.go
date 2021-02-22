1b5dfb0f48464d81daf0198ffcab272f81622e95
Tacio Costa <taciomcosta@gmail.com>
2021-02-22 20:19:48 -0300

    style: lint stub writer

--- internal/usecases/mocks/stub_failing_writer.go
+++ internal/usecases/mocks/stub_failing_writer.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 
 	"github.com/taciomcosta/kronos/internal/entities"
-	uc "github.com/taciomcosta/kronos/internal/usecases"
 )
 
 // NewFailingWriter stubs a writer that always returns error
@@ -13,10 +12,7 @@ func NewFailingWriter() *StubFailingWriter {
 }
 
 // StubFailingWriter implements entities.Writer for tests purposes
-type StubFailingWriter struct {
-	jobs         []entities.Job
-	jobsResponse uc.FindJobsResponse
-}
+type StubFailingWriter struct{}
 
 // CreateJob creates a job.
 func (mr *StubFailingWriter) CreateJob(job *entities.Job) error {
