a8ad226dc848a8479ad9a79d206ab475a309b23c
Tacio Costa <taciomcosta@gmail.com>
2021-02-25 19:21:14 -0300

    fix: testing another path for brew service

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,7 @@ import (
 // EnableDefaultMode reads config.json file or uses default configuration
 func EnableDefaultMode() error {
 	viper.SetConfigType("json")
-	viper.SetDefault("db", "/var/lib/kronos/kronos.db")
+	viper.SetDefault("db", "/usr/local/var/kronos/kronos.db")
 	viper.SetDefault("host", ":8080")
 	viper.AddConfigPath(getConfigFilePath())
 	err := viper.ReadInConfig()
@@ -15,7 +15,7 @@ func EnableDefaultMode() error {
 }
 
 func getConfigFilePath() string {
-	return "/etc/kronos/"
+	return "/usr/local/etc/kronos/"
 }
 
 // EnableTestMode sets test configuration as current
