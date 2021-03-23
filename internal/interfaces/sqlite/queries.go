9c9d32c7f37edbc2e4fcaa9733aff44f5a35c156
Tacio Costa <taciomcosta@gmail.com>
2021-03-23 12:13:07 -0300

    feat: add tables notifier, slack

--- internal/interfaces/sqlite/queries.go
+++ internal/interfaces/sqlite/queries.go
@@ -1,10 +1,27 @@
 package sqlite
 
 var tablesStmts []string = []string{
-	`CREATE TABLE IF NOT EXISTS job(name TEXT PRIMARY KEY, command TEXT, tick TEXT, status BOOLEAN)`,
+	`CREATE TABLE IF NOT EXISTS job(
+		name TEXT PRIMARY KEY,
+		command TEXT,
+		tick TEXT,
+		status BOOLEAN
+	)`,
 	`CREATE TABLE IF NOT EXISTS execution(
-		job_name TEXT, date DATE, STATUS TEXT, 
-		mem_usage INTEGER, cpu_time INTEGER
+		job_name TEXT,
+		date DATE,
+		STATUS TEXT,
+		mem_usage INTEGER,
+		cpu_time INTEGER
+	)`,
+	`CREATE TABLE IF NOT EXISTS notifier(
+		name TEXT PRIMARY KEY,
+		type INTEGER
+	)`,
+	`CREATE TABLE IF NOT EXISTS slack(
+		auth_token TEXT,
+		channel_ids TEXT,
+		notifier_name TEXT REFERENCES notifier(name)
 	)`,
 }
 
