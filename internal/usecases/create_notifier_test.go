4ba47639533b9ab27fc7cb6a91ef26393a6f52f6
Tacio Costa <taciomcosta@gmail.com>
2021-03-25 19:16:33 -0300

    tests: refactor create notifier

--- internal/usecases/create_notifier_test.go
+++ internal/usecases/create_notifier_test.go
@@ -9,31 +9,46 @@ import (
 	"github.com/taciomcosta/kronos/internal/usecases/mocks"
 )
 
-func TestCreateNotifier(t *testing.T) {
-	writerReader := mocks.NewStubWriterReader()
-	uc.New(writerReader, writerReader, mocks.NewSpyHost())
-	request := uc.CreateNotifierRequest{
-		Name: "mynotifier",
-		Type: entities.SlackNotifierType,
-		Metadata: map[string]string{
-			"auth_token":  "123",
-			"channel_ids": "1,2,3",
+var testsCreateNotifiers = []struct {
+	request  uc.CreateNotifierRequest
+	response uc.CreateNotifierResponse
+	err      error
+	writer   uc.Writer
+}{
+	{
+		request: uc.CreateNotifierRequest{
+			Name: "mynotifier",
+			Type: entities.SlackNotifierType,
+			Metadata: map[string]string{
+				"auth_token":  "123",
+				"channel_ids": "1,2,3",
+			},
 		},
-	}
-	got, err := uc.CreateNotifier(request)
-	want := uc.CreateNotifierResponse{Msg: "mynotifier created"}
-	assertEqual(t, got, want)
-	assertError(t, err, nil)
+		response: uc.CreateNotifierResponse{Msg: "mynotifier created"},
+		err:      nil,
+		writer:   mocks.NewStubWriterReader(),
+	},
+	{
+		request: uc.CreateNotifierRequest{
+			Name: "mynotifier",
+			Type: entities.SlackNotifierType,
+			Metadata: map[string]string{
+				"auth_token":  "123",
+				"channel_ids": "1,2,3",
+			},
+		},
+		response: uc.CreateNotifierResponse{},
+		err:      errors.New("StubFailingWriter"),
+		writer:   mocks.NewFailingWriter(),
+	},
 }
 
-func TestCreateNotifierFailure(t *testing.T) {
-	writer := mocks.NewFailingWriter()
-	reader := mocks.NewStubWriterReader()
-	uc.New(writer, reader, mocks.NewSpyHost())
-	request := uc.CreateNotifierRequest{}
-	got, err := uc.CreateNotifier(request)
-	want := uc.CreateNotifierResponse{}
-	wantErr := errors.New("StubFailingWriter")
-	assertEqual(t, got, want)
-	assertError(t, err, wantErr)
+func TestCreateNotifier(t *testing.T) {
+	for _, tt := range testsCreateNotifiers {
+		reader := mocks.NewStubWriterReader()
+		uc.New(tt.writer, reader, mocks.NewSpyHost())
+		response, err := uc.CreateNotifier(tt.request)
+		assertEqual(t, response, tt.response)
+		assertError(t, err, tt.err)
+	}
 }
