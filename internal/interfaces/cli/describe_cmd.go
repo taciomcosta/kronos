e03444743488b5f41598e08da95d884340216320
Tacio Costa <taciomcosta@gmail.com>
2021-03-31 17:54:44 -0300

    feat: add describe notifier cmd

--- internal/interfaces/cli/describe_cmd.go
+++ internal/interfaces/cli/describe_cmd.go
@@ -1,13 +1,14 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 
 	uc "github.com/taciomcosta/kronos/internal/usecases"
 )
 
-var describeJobTemplate = `
-Name: %s
+var describeJobTemplate = `Name: %s
 Command: %s
 Tick: %s
 Last Execution: %s
@@ -43,6 +44,36 @@ var describeJobCmd = &cobra.Command{
 	},
 }
 
+var describeNotifierTemplate = `Name: %s
+Type: %s
+Metadata:
+%s
+`
+
+var describeNotifierCmd = &cobra.Command{
+	Use:   "notifier",
+	Short: "Describe a notifier",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+		r := uc.DescribeNotifierResponse{}
+		err := client.get("/notifiers/"+name, &r)
+		out.error(err)
+		out.printf(
+			describeNotifierTemplate,
+			r.Name, r.Type, formatMetadata(r.Metadata))
+	},
+}
+
+func formatMetadata(metadata map[string]string) string {
+	var formatted string
+	for key, value := range metadata {
+		formatted += fmt.Sprintf("- %s: %s\n", key, value)
+	}
+	return formatted
+}
+
 func init() {
 	describeCmd.AddCommand(describeJobCmd)
+	describeCmd.AddCommand(describeNotifierCmd)
 }
