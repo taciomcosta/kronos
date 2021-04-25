8147bbfe81b3b0bbfb895e4b0a6b042c9940a130
Tacio Costa <taciomcosta@gmail.com>
2021-04-25 10:54:48 -0300

    tests: add spy_notifier_service to mocker interface

--- internal/usecases/mocker/dependency_builder.go
+++ internal/usecases/mocker/dependency_builder.go
@@ -3,24 +3,27 @@ package mocker
 import (
 	uc "github.com/taciomcosta/kronos/internal/usecases"
 	spyhost "github.com/taciomcosta/kronos/internal/usecases/mocker/spy_host"
+	spynotifierservice "github.com/taciomcosta/kronos/internal/usecases/mocker/spy_notifier_service"
 	stubreader "github.com/taciomcosta/kronos/internal/usecases/mocker/stub_reader"
 	stubwriter "github.com/taciomcosta/kronos/internal/usecases/mocker/stub_writer"
 )
 
 // DependencyBuilder builds both stubs and spy dependencies
 type DependencyBuilder struct {
-	stubReaderBuilder *stubreader.Builder
-	stubWriterBuilder *stubwriter.Builder
-	spyHostBuilder    *spyhost.Builder
+	stubReaderBuilder         *stubreader.Builder
+	stubWriterBuilder         *stubwriter.Builder
+	spyHostBuilder            *spyhost.Builder
+	spyNotifierServiceBuilder *spynotifierservice.Builder
 }
 
 // BuildDependencies ...
 // TODO: rename to BuildDefault()
 func (d *DependencyBuilder) BuildDependencies() uc.Dependencies {
 	return uc.Dependencies{
-		Reader: d.stubReaderBuilder.Build(),
-		Writer: d.stubWriterBuilder.Build(),
-		Host:   d.spyHostBuilder.Build(),
+		Reader:          d.stubReaderBuilder.Build(),
+		Writer:          d.stubWriterBuilder.Build(),
+		Host:            d.spyHostBuilder.Build(),
+		NotifierService: d.spyNotifierServiceBuilder.Build(),
 	}
 }
 
@@ -40,4 +43,10 @@ func (d *DependencyBuilder) Writer() *stubwriter.Builder {
 func (d *DependencyBuilder) Host() *spyhost.Builder {
 	d.spyHostBuilder = spyhost.NewSpyHostBuilder(d)
 	return d.spyHostBuilder
+}
+
+// NotifierService ...
+func (d *DependencyBuilder) NotifierService() *spynotifierservice.Builder {
+	d.spyNotifierServiceBuilder = spynotifierservice.NewSpyNotifierServiceBuilder(d)
+	return d.spyNotifierServiceBuilder
 }
